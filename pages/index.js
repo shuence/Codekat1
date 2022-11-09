4c8b2c4077a369f0be82cee0d83b76ca5fa2654a
Shubham Pitekar <shubhampitekar98@gmail.com>
2022-11-09 07:59:32 +0000

    Completed Header

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header"
+import Banner from "../components/banner"
+
+export default function Home() {
+  const styles ={}
+  return (
+    <div>
+    <Header/>
+    <Banner/>
+    </div>
+  )
+}
+
